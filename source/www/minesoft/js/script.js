099d48d37f679c2f02099f0139a8d8a3166223ab
nianqinianyi <nianqinianyi@163.com>
2018-08-05 22:08:50 +0800

    minesoft 和 sixlab 的首页  $(hexo)

---
+++ source/www/minesoft/js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+	/* ---- Countup timer ---- */
+
+	$('#counter').countup({
+		timestamp : 1527609600000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+});
