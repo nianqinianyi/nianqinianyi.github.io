1ecb1f8f6d97b6fe4d57f4fb40a42bbc1d2516a1
nianqinianyi <nianqinianyi@163.com>
2025-06-04 13:51:14 +0800

    将这三个配置内容多的，放到单独文件中去

---
+++ .vitepress/nav.ts
@@ -0,0 +1,11 @@
+export default [
+    { text: '首页', link: '/' },
+    { text: '文章', link: '/archives/' },
+    {
+        text: '测试', items: [
+            { text: '测试1', link: '/test1' },
+            { text: '测试2', link: '/test2' }
+        ]
+    },
+    { text: '关于', link: '/about' }
+]
---
+++ .vitepress/sidebar.ts
@@ -0,0 +1,12 @@
+export default {
+    "/archives/": [
+        {
+            text: 'Examples',
+            items: [
+                { text: 'Markdown Examples', link: '/examples/' },
+                { text: 'Runtime API Examples', link: '/examples/api-examples' },
+                { text: 'Demo Examples', link: '/examples/demo' },
+            ]
+        }
+    ]
+}
